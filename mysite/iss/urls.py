9e430b9f14650942378764ab51f519413a205a1f
prakhar1144 <prakhar841301@gmail.com>
2020-09-09 22:17:19 +0530

    base.html style.css live.css url views are created

--- mysite/iss/urls.py
+++ mysite/iss/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'iss'
+
 urlpatterns = [
-	path('', views.index, name='index')
+	path('', views.home, name='home'),
+	path('live/', views.live, name='live')
 
 ]
--- mysite/iss/views.py
+++ mysite/iss/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-	return render(request, 'iss/index.html')
+def home(request):
+	return render(request, 'iss/home.html')
+
+def live(request):
+	return render(request, 'iss/live.html')
